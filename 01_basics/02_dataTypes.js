4b1fc08ff17a6b9fb6fe31f1764cc3a88ca001c5
Saswati Choudhury <146979103+saswati-26@users.noreply.github.com>
2024-06-18 18:21:49 +0000

    dataTypes and ECMA standards

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,40 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs; not browser
+//semicolon can be avoided in react, node files
+// important: code readability
+
+// console.log(3 + 3) console.log("Puchuu");
+/*
+console.log(3
+    +
+        3) //code readability should be high
+*/
+console.log(3 + 3); console.log("Puchuu");
+
+console.log(3 + 3)
+console.log("Puchuu");
+
+//official documentation- https://tc39.es/ecma262/#sec-intro
+//ecmaScript writes standards and js codes
+
+
+let name = "Puchuu"
+let age = 20
+let isLoggedIn = false
+let state = null
+
+// number => 2 to the power of 53
+// bigint => for long numbers
+// string => ""
+//boolean => true / false
+// null => standalone value; value is empty not undefined
+// undefined => value not defined but variable is declared
+// symbol => unique
+
+//object 
+
+console.log(typeof "puchuu");
+console.log(typeof age);
+console.log(typeof undefined);   //undefined
+console.log(typeof null);       //object // js error maybe
