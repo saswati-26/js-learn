a6776b27667d9e99429ad258dff86efd4f3f77ad
Saswati Choudhury <146979103+saswati-26@users.noreply.github.com>
2024-06-19 10:26:54 +0000

    Some basic operations

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -48,3 +48,52 @@ console.log(booleanIsLoggedIn);
 let  someNumber = 26
 let stringNumber = String(someNumber)
 console.log([typeof stringNumber, stringNumber]);
+
+
+// ******************** Operations ********************
+
+let value = 4
+let negValue = -value
+console.log(negValue);
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 3);
+console.log(2 / 2);
+console.log(2 % 3);
+
+let str1 = "hello"
+let str2 = " puchuu"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("2" + 6);
+console.log(2 + "6");
+console.log("8" + 2 + 6);
+console.log(2 + 6 + "8");
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(true);   //true
+console.log(+true);  // 1
+// console.log(true+);  //error
+console.log(+"");   //0
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2  // code readability absent
+
+let gameCounter = 100
+// gameCounter++; //postfix //101 // assign then increment
+++gameCounter;  //prefix    //101 // increment then assign
+console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+console.log(`x: ${x}, y: ${y}`);    //x: 4, y: 3
+
+let a = 3;
+let b = ++a;
+console.log(`a: ${a}, b: ${b}`);    //a: 4, b: 4
+
+// link to study=> https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
