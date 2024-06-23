25f17ebe55a61477cfb67ea3844286a70eda64e3
Saswati Choudhury <146979103+saswati-26@users.noreply.github.com>
2024-06-23 14:00:08 +0000

    arrays part 2

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -38,4 +38,5 @@ console.log(Array.from({name: "pucuu"})); // [] // cannot convert directly have 
 let score1 = 100
 let score2 = 200
 let score3 = 300
+
 console.log(Array.of(score1, score2, score3)); // returns a new array from a set of elemsnt // [ 100, 200, 300 ]
